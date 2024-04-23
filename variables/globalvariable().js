6a09f1f268eeb1e768f0d576ae28e645b30a9104
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-04-23 13:45:32 +0530

    Create globalvariable().js

---
+++ variables/globalvariable().js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="demo.js"></script>
+</head>
+<body>
+    <p>Wel come to Javascript </p>
+    <form>
+        <input type="button" value="Click me" onclick="msg()">
+    </form>
+    <script>
+        var value=200
+        function a()
+        {
+            alert(value)
+        }
+        function b()
+        {
+            alert(value)
+        }
+        a();
+
+    </script>
+
+   
+</body>
+</html>
