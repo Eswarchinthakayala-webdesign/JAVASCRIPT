10c9721d4340739d7676b6394b34a251266bd72c
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-05-06 22:41:12 +0530

    Create demo.js

---
+++ fundamentals part 1/variables&values/demo.js
@@ -0,0 +1,26 @@
+//values
+console.log('jonas')
+console.log(23)
+//variables
+let firstName="Eswar";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+/*
+use camelCase Represent Variables like
+a.FirstName,
+b.First_name,
+c.FirstNamePerson,
+Rules
+1.variables not start with integer
+  let 3years=3 ;
+2.Don't use special characters in the variables except '$'
+  let first&last="fl";
+3.Don't Use new,function AS variable Because it is reserved Keyword
+  let new=27;
+  let function=23;
+4.Don't start variable with UpperCase
+    */
+let myFirstJob='Programmer';
+console.log(myFirstjob);
