96f008c8b804cef2a18e8af84c8a81a8dade6e38
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-05-06 23:52:00 +0530

    Create script.js

---
+++ fundamentals part 1/datatypes/script.js
@@ -0,0 +1,26 @@
+//data types
+//1.Boolean Values
+let javascriptIsFun=true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun)
+//Number
+let age=23;
+console.log(age);
+console.log(typeof age);
+//strings
+let firstName='Eswar';
+console.log(typeof firstName);
+//changing the datatype
+javascriptIsFun="ram";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+//undefined
+let year;
+console.log(year);
+console.log(typeof year);
+year=1991
+console.log(year)
+//Null-bug in JS
+console.log(typeof null)
