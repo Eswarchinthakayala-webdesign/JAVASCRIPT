309698a8275b4e290f5b7e692e56def138346574
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-05-07 00:05:23 +0530

    Create script.js

---
+++ fundamentals part 1/declaring varibles/script.js
@@ -0,0 +1,12 @@
+//using let we can mutate the values
+let age=30;
+age=31;
+//using Const we can't mutate the values
+//we can't declare empty const like const year;
+const birthYear=1991;
+//var can be mutate
+var firstName='eswar';
+firstName='sriram';
+//without using let,const & var
+lastName="chinthakayala";
+console.log(lastName);
