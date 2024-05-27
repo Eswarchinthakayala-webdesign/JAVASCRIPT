4008e606150477b8607e791a986f603baf44ce27
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-05-28 03:28:11 +0530

    Create script.js

---
+++ strings and templates literals/script.js
@@ -0,0 +1,23 @@
+//strings and templates
+const firstName="jonas";
+const job="teacher";
+const birthYear=1991;
+const year=2037;
+
+const jonas="I 'm "+firstName+',a ' +(year-birthYear)+' years old '+job+'!';
+console.log(jonas);
+
+//string template literals
+
+
+const jonasNew=`I'm ${firstName},a ${(year-birthYear)} years old ${job}!`;
+console.log(jonasNew);
+
+console.log(`i am noob in programming`);
+
+//new line
+
+console.log(' i am a programmer \n\ i know javascript');
+
+
+
