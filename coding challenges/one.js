8a29da3d887ead54ba11cb4ac44fdfacee052d0b
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-05-28 03:08:51 +0530

    Create one.js

---
+++ coding challenges/one.js
@@ -0,0 +1,12 @@
+//Coding Challenge-1
+
+const marksWeight=78;
+const marksHeight=1.69;
+const jonasWeight=92;
+const jonasHeight=1.95;
+
+const jonasBMI=jonasWeight/(jonasHeight**2);
+const marksBMI=marksWeight/(marksHeight*marksHeight);
+
+console.log(marksBMI>jonasBMI);
+
