38989b95099141006ab08179ca080af4d2a96e7a
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-05-29 01:45:13 +0530

    Create script.js

---
+++ Boolean logic/script.js
@@ -0,0 +1,14 @@
+const age1=18;
+const age2=18;
+
+if(age1==18 && age2==18)
+    {
+        console.log("yes!")
+    }
+const hasDriverLicense=true;
+const hasGodVision=false;
+console.log(hasDriverLicense&&hasGodVision);
+console.log(hasDriverLicense||hasGodVision);
+console.log(!hasDriverLicense);
+
+
