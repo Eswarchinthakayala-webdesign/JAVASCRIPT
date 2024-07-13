a21712ebc241eaa6d93021f731c9a2990e885aad
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-07-13 21:27:12 +0530

    Create Arrow functions.js

---
+++ Intermediate Level/Arrow functions.js
@@ -0,0 +1,17 @@
+// traditional functions
+
+function greet(username)
+{
+    return `Hello ${username}`
+}
+
+console.log(greet("Developer"));
+
+// Arrow functions
+
+greet=(username)=>
+{
+    return `Hello ${username}`
+}
+
+console.log(greet("Arrow function"));
