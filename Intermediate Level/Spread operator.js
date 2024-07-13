8f944dbd6b5f5a6b22b875c05a8f02fcdbbceb0f
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-07-13 21:23:50 +0530

    Create Spread operator.js

---
+++ Intermediate Level/Spread operator.js
@@ -0,0 +1,12 @@
+
+// Spread operators
+function giveMe4(a,b,c,d)
+{
+    console.log("a",a);
+    console.log("b",b);
+    console.log("c",c);
+    console.log("d",d);
+}
+ const color=['red','blue','green','yellow']
+
+giveMe4(...color);
