30acca31f3f3cfa4fd6f266bf5d704cf89ab7f30
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-07-13 22:29:20 +0530

    Update Spread operator.js

--- Intermediate Level/Spread operator.js
+++ Intermediate Level/Spread operator.js
@@ -10,3 +10,51 @@ function giveMe4(a,b,c,d)
  const color=['red','blue','green','yellow']
 
 giveMe4(...color);
+
+
+// Example -2
+
+// using arrays
+const strNums=['one','two','three'];
+const moreStrNums=['four','five','six']
+
+const concat=[...strNums,...moreStrNums];
+
+console.log(concat)
+
+let peoples=['eswar','sriram','vamsi'];
+const allPeoples=['raju',...peoples,'rajesh'];
+
+console.log(allPeoples.length)
+console.log(allPeoples)
+
+// using objects
+
+const obj1=
+{
+    x:1,
+    y:2
+}
+
+const obj2=
+{
+    z:3
+}
+
+const obj3={
+    ...obj1,...obj2
+}
+
+console.log(obj3)
+
+// Example-3
+
+let person=
+{
+    name:'eswar',
+    age:25,
+    gender:"male"
+}
+
+const clone={...person,work:"developer"}
+console.log(clone)
