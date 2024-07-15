c906f212af327f75a7d66010e2aa3fd3916e22be
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-07-15 19:15:26 +0530

    Update object destructuring.js

--- Intermediate Level/object destructuring.js
+++ Intermediate Level/object destructuring.js
@@ -8,3 +8,8 @@ const student=
 
 const {name}=student;
 console.log(name);
+// changing the label in destructuring for objects
+
+const nums={x:100,y:200};
+const {x:num1,y:num2}=nums;
+console.log(num1,num2)
