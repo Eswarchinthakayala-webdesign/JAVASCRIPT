d3d44dcf3037940d6da7c920b0c943722e7a9a9d
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-07-16 19:29:53 +0530

    Create for of loop.js

---
+++ Intermediate Level/for of loop.js
@@ -0,0 +1,26 @@
+
+// for of loop in javascript
+
+// Array
+
+let people=['eswar','sriram','vamsi'];
+
+for(let person in people )
+{
+    console.log(person);
+}
+
+// text
+const text="Eswar";
+for(let letter of text)
+{
+    console.log(letter);
+}
+
+// Example
+
+const array1=['a','b','c'];
+for(let letter of array1)
+{
+    console.log(letter);
+}
