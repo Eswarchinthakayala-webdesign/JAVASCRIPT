be1b424a811e3276fbca6c015c7220cc3a913cf4
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-07-16 19:19:06 +0530

    Create for in loop.js

---
+++ Intermediate Level/for in loop.js
@@ -0,0 +1,20 @@
+
+// fo in loop
+let person=
+{
+    name:"eswar",
+    age:21,
+    gender:"male",
+}
+for(let keys in person)
+{
+
+   console.log(keys,person[keys]);
+}
+// Example two
+
+let list=['one','two','three','four'];
+for(let index in list)
+{
+    console.log(index,list[index]);
+}
